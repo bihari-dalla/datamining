e03ad74c9d58cc224f6974c77130a5c519eb562e
bihari-dalla <firstyeardatascience@gmail.com>
2026-04-02 08:43:48 +0530

    decisiontree.py

---
+++ decisiontree.py
@@ -0,0 +1,25 @@
+if(!require(rpart)) install.packages("rpart")
+if(!require(rpart.plot)) install.packages("rpart.plot")
+
+library(rpart)
+library(rpart.plot)
+
+data(iris)
+
+iris$Species <- as.factor(iris$Species)
+
+set.seed(123)
+index <- sample(1:nrow(iris), 0.7 * nrow(iris))
+train <- iris[index, ]
+test <- iris[-index, ]
+
+model <- rpart(Species ~ ., data = train, method = "class")
+
+rpart.plot(model)
+
+pred <- predict(model, test, type = "class")
+
+table(Predicted = pred, Actual = test$Species)
+
+accuracy <- mean(pred == test$Species)
+accuracy
