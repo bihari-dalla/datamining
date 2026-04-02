47da1115dc7cccca322859ad69182d8e64a58a99
bihari-dalla <firstyeardatascience@gmail.com>
2026-04-02 12:26:51 +0530

    dmall.py

---
+++ dmall.py
@@ -0,0 +1,17 @@
+'''
+p1 : https://tinyurl.com/dm-weka1
+p2(1) : https://tinyurl.com/dm-decisiontree
+p2(2) : https://tinyurl.com/dm-naivebayes
+p3 : https://tinyurl.com/dm-clustering
+p4 : https://tinyurl.com/dm-agriclustering
+p5(1) : https://tinyurl.com/dm-kmean
+p5(2) : https://tinyurl.com/dm-agglomerative
+p5(3) : https://tinyurl.com/dm-clustering2
+p6(1) : https://tinyurl.com/dm-knnnn
+p6(2) : https://tinyurl.com/dm-knnpy
+p7(1) : https://tinyurl.com/dm-aprioricsv
+p7(2) : https://tinyurl.com/dm-apriroilist
+p9(1) : https://tinyurl.com/dm-edadf
+p9(2) : https://tinyurl.com/dm-edacsv
+p10 : https://tinyurl.com/dm-webmining
+'''
